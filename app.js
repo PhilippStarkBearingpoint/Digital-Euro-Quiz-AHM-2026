5554c49191f77de4eb1a02ab1f592e2dc9ae00f6
PhilippStarkBearingpoint <philipp.stark@bearingpoint.com>
2026-06-09 15:49:33 +0200

    Update app.js

--- app.js
+++ app.js
@@ -12,7 +12,7 @@
   const firebaseConfig = {
     apiKey: 'AIzaSyDloOpiwDq7PzwkhBiOap8SCncXn226UNo',
     authDomain: 'digital-euro-quiz-ahm-2026.firebaseapp.com',
-    databaseURL: 'https://digital-euro-quiz-ahm-2026-default-rtdb.europe-west1.firebasedatabase.app',
+    databaseURL: 'https://digital-euro-quiz-ahm-2026-default-rtdb.europe-west1.firebasedatabase.app/',
     projectId: 'digital-euro-quiz-ahm-2026',
     storageBucket: 'digital-euro-quiz-ahm-2026.firebasestorage.app',
     messagingSenderId: '667467540583',
